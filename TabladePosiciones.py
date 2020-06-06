513a4b782c49f6ba8c07f6fae9c3d5aa5c7359bb
MarcoSilva <62036248+marcosilva1998@users.noreply.github.com>
2020-06-06 17:24:33 -0300

    Update TabladePosiciones.py

--- TabladePosiciones.py
+++ TabladePosiciones.py
@@ -1,56 +1,33 @@
-import PySimpleGUI as sg 
+import PySimpleGUI as sg
 sg.ChangeLookAndFeel('BrownBlue') # change style
-
-WIN_W: int = 90
-WIN_H: int = 25
 filename:str = None
-
 # string variables to shorten loop and menu code
-Total: str = 'Total'
-Facil: str = 'Facil'
-Medio: str = 'Medio'
-Dificil: str = 'Dificil'
-Salir: str = 'Salir'
+menu_layout: list = ['Total','Facil','Medio','Dificil','Salir']
 
-menu_layout: list = [[Total],
-                     [Facil],
-                     [Medio],
-                     [Dificil],
-                     [Salir]]
-
-layout: list = [[sg.Menu(menu_layout)],
-                [sg.Text('', font=('Consolas', 10), size=(WIN_W, 1), key='_INFO_')],
-                [sg.Multiline(font=('Consolas', 12), size=(WIN_W, WIN_H), key='_BODY_')]]
+layout: list = [[sg.Button(menu_layout[0]),sg.Button(menu_layout[1]),sg.Button(menu_layout[2]),sg.Button(menu_layout[3]),sg.Button(menu_layout[4])],
+                [sg.Text('', font=('Consolas', 10), size=(90, 1), key='_INFO_')],
+                [sg.Text(font=('Consolas', 12), size=(90, 25), key='_BODY_')]]
 
 window: object = sg.Window('Top', layout=layout, margins=(0, 0), resizable=True, return_keyboard_events=True)
-window.read(timeout=1)
-window.maximize()
-window['_BODY_'].expand(expand_x=True, expand_y=True)
-
-
-def open_file() -> str:
-    ''' Open file and update the infobar '''
-    try:
-        filename: str = sg.popup_get_file('Open File', no_window=True)
-    except:
-        return
-    if filename not in (None, '') and not isinstance(filename, tuple):
-        with open(filename, 'r') as f:
-            window['_BODY_'].update(value=f.read())
-        window['_INFO_'].update(value=filename)
-    return filename
-
-
+Tabla=window.FindElement('_BODY_')
 while True:
     event, values = window.read()
-
-    if event in (Salir, 'o:79'):
+    print(event)
+    if event in ('Salir',None):
         break
-    if event in (Total, 'o:79'):
-        filename = open_file(Puntajes_Total.txt)
-    if event in (Facil, 'o:79'):
-        filename = open_file(Puntajes_Facil.txt)
-    if event in (Medio, 'o:79'):
-        filename = open_file(Puntajes_Medio.txt)
-    if event in (Dificil, 'o:79'):
-        filename = open_file(Puntajes_Dificil.txt)
+    elif event == 'Total':
+        filename = open('Puntajes_Total.txt','r')
+        Tabla.Update(filename.read())
+        filename.close()
+    elif event == 'Facil':
+        filename = open('Puntajes_Facil.txt','r')
+        Tabla.Update(filename.read())
+        filename.close()
+    elif event == 'Medio':
+        filename = open('Puntajes_Medio.txt','r')
+        Tabla.Update(filename.read())
+        filename.close()
+    elif event == 'Dificil':
+        filename = open('Puntajes_Dificil.txt','r')
+        Tabla.Update(filename.read())
+        filename.close()
