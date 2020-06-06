a6baeed91a0f6432355c8ec2464685825bd3993a
MarcoSilva <62036248+marcosilva1998@users.noreply.github.com>
2020-06-06 18:42:26 -0300

    Ultimas modificaciones para que anden correctamente

--- TabladePosiciones.py
+++ TabladePosiciones.py
@@ -1,33 +1,38 @@
 import PySimpleGUI as sg
 sg.ChangeLookAndFeel('BrownBlue') # change style
-filename:str = None
+def genero_Top():
+    filename:str = None
 # string variables to shorten loop and menu code
-menu_layout: list = ['Total','Facil','Medio','Dificil','Salir']
+    menu_layout: list = ['Total','Facil','Medio','Dificil','Salir']
 
-layout: list = [[sg.Button(menu_layout[0]),sg.Button(menu_layout[1]),sg.Button(menu_layout[2]),sg.Button(menu_layout[3]),sg.Button(menu_layout[4])],
-                [sg.Text('', font=('Consolas', 10), size=(90, 1), key='_INFO_')],
-                [sg.Text(font=('Consolas', 12), size=(90, 25), key='_BODY_')]]
+    layout: list = [[sg.Button(menu_layout[0]),sg.Button(menu_layout[1]),sg.Button(menu_layout[2]),sg.Button(menu_layout[3]),sg.Button(menu_layout[4])],
+                    [sg.Text('', font=('Consolas', 10), size=(90, 1), key='_INFO_')],
+                    [sg.Text(font=('Consolas', 12), size=(90, 25), key='_BODY_')]]
 
-window: object = sg.Window('Top', layout=layout, margins=(0, 0), resizable=True, return_keyboard_events=True)
-Tabla=window.FindElement('_BODY_')
-while True:
-    event, values = window.read()
-    print(event)
-    if event in ('Salir',None):
-        break
-    elif event == 'Total':
-        filename = open('Puntajes_Total.txt','r')
-        Tabla.Update(filename.read())
-        filename.close()
-    elif event == 'Facil':
-        filename = open('Puntajes_Facil.txt','r')
-        Tabla.Update(filename.read())
-        filename.close()
-    elif event == 'Medio':
-        filename = open('Puntajes_Medio.txt','r')
-        Tabla.Update(filename.read())
-        filename.close()
-    elif event == 'Dificil':
-        filename = open('Puntajes_Dificil.txt','r')
-        Tabla.Update(filename.read())
-        filename.close()
+    window: object = sg.Window('Top', layout=layout, margins=(0, 0), resizable=True, return_keyboard_events=True)
+    Tabla=window.FindElement('_BODY_')
+    while True:
+        event, values = window.read()
+        if event in ('Salir',None):
+            break
+        elif event == 'Total':
+            filename = open('Puntajes_Total.txt','r')
+            Tabla.Update(filename.read())
+            filename.close()
+        elif event == 'Facil':
+            filename = open('Puntajes_Facil.txt','r')
+            Tabla.Update(filename.read())
+            filename.close()
+        elif event == 'Medio':
+            filename = open('Puntajes_Medio.txt','r')
+            Tabla.Update(filename.read())
+            filename.close()
+        elif event == 'Dificil':
+            filename = open('Puntajes_Dificil.txt','r')
+            Tabla.Update(filename.read())
+            filename.close()
+    window.close()
+    return(event)
+#ProgramaPrincipal-------------
+if __name__ == "__main__":
+    genero_Top()
--- pantalla_Principal.py
+++ pantalla_Principal.py
@@ -1,10 +1,11 @@
 import json
 import PySimpleGUI as sg
 from Opciones import Ventana_Opciones
+from TabladePosiciones import genero_Top
 import csv
 
 #Aca Arranca La Pantalla Principal----------------------------------------------------------------------------------------------------------------------------------------------
-def obtengo_Perfil():
+def obtengo_Perfil():                                #Busco el nombre del ultimo usuario registrado
     archivo_csv=open('Archivo_Opciones.csv','r')
     perfiles=csv.reader(archivo_csv)
     for perfil in perfiles:
@@ -18,7 +19,7 @@ def establezco_PP(nombre):
     columna_izquierda=[     [sg.Text("USUARIO:"+nombre)],  #Tomar el nombre del usuario del archivo que se deberia generar desde el menu opciones
                             [sg.Button(button_text="JUGAR",size=(40,4),pad=(0,20))],
                             [sg.Button(button_text="OPCIONES",size=(40,4),pad=(0,20))],
-                            [sg.Button(button_text="VER TOP 10",size=(40,4),pad=(0,20))],                          ]
+                            [sg.Button(button_text="VER TOP 10",size=(40,4),pad=(0,20))],            ]
 
     columna_derecha=[       [sg.Text("ESCRABBLE-AR",font=("default",40),pad=(130,0))],
                             [sg.Button(button_text="SALIR",size=(10,3),pad=((550,0),(510,0)))]       ]
@@ -33,24 +34,25 @@ def establezco_PP(nombre):
     while True:
         if boton_cliqueado in(None,'SALIR'):
             break
-        if boton_cliqueado == 'JUGAR':
+        elif boton_cliqueado == 'JUGAR':
+            #Aca abro la nueva ventana en el mismo lugar que la anterior , luego de cerrar la principal
             print(boton_cliqueado)
             window.close()
-        #Aca abro la nueva ventana en el mismo lugar que la anterior , luego de cerrar la principal
             break
         elif boton_cliqueado =='VER TOP 10':
-            print(boton_cliqueado)
-        #Aca abro una ventana al lado de la principal
+            #Aca abro la nueva ventana en el mismo lugar que la anterior , luego de cerrar la principal
+            window.close()
+            if(genero_Top()in(None,'Salir')):
+                establezco_PP(obtengo_Perfil())
             break
         elif boton_cliqueado =='OPCIONES':
+            #Aca abro la nueva ventana en el mismo lugar que la anterior , luego de cerrar la principal
             window.close()
-            if(Ventana_Opciones()=='Salir'):
+            if(Ventana_Opciones()in(None,'Salir')):
                 establezco_PP(obtengo_Perfil())
-
-        #Aca abro la nueva ventana en el mismo lugar que la anterior , luego de cerrar la principal
             break
     return()
 
-#Programa Principal-----------------------------------------------
+#Programa Principal-----------------------------------------------------------------------------------------------
 if __name__ == "__main__":
     establezco_PP(obtengo_Perfil())
