1b8b3870f86ea52705add1161b1da9b10a54a953
MarcoSilva <62036248+marcosilva1998@users.noreply.github.com>
2020-06-15 18:48:10 -0300

    Update MarcodePartida.py

--- MarcodePartida.py
+++ MarcodePartida.py
@@ -10,47 +10,47 @@ def generar_Pantalla():
     arch = open('Archivo_Opciones.csv','r')
     reader = csv.reader(arch)
 
-    layout: list = [[sg.sg.Button(Pausar),sg.Button(Rendirse),sg.Button(Salir)]
+    layout: list = [[sg.Button(Pausar),sg.Button(Rendirse),sg.Button(Salir)]]
 
     window: object = sg.Window('Top', layout=layout, margins=(0, 0), resizable=True, return_keyboard_events=True)
-    
+
     #run = raw_input("Start? > ")
-    def Timer(run,pausar,terminar)
+    def Timer(run,pausar,terminar):
       secs = 0
       if run == "start":
-      while (secs != 30) && (terminar != True):
-        if (pausar = True):
-          print(">>>>>>>>>>>>>>>>>>>>> {}".format(secs))
-          time.sleep(1)
-          secs += 1
-  
+          while (secs != 30) & (terminar != True):
+              if (pausar == True):
+                  print(">>>>>>>>>>>>>>>>>>>>> {}".format(secs))
+                  time.sleep(1)
+                  secs += 1
+
     def modificar_dato(filename,puntaje):
       filas = 10
       fila = 1
       columna
       contenido = list()
       contenido = filename.readlines()
-          for fila in filas:
-              columnas = contenido[fila-1].split(';')
-              columnas[4] = nuevo_dato
-              contenido[fila-1] = ';'.join(columnas)+ '\n'
+      for fila in filas:
+         columnas = contenido[fila-1].split(';')
+         columnas[4] = nuevo_dato
+         contenido[fila-1] = ';'.join(columnas)+ '\n'
       with open(ruta, 'w') as filename:
           archivo.writelines(contenido)
-    
+
     while True:
         event, values = window.read()
-          if event in ('Pausar',None):
-           layout = [[sg.Text('¿Reanudar?')],
-                     [, sg.Stop()]]      
+        if event in ('Pausar',None):
+            layout = [[sg.Text('¿Reanudar?')],
+                     [sg.Stop()]]
             window = sg.Window('Pausa', layout)
-         if event in ('Rendirse',None):
+        if event in ('Rendirse',None):
            layout = [[sg.Text('¿Arrojas la toalla?')],
-                     [sg.exit(), sg.Cancel()]]      
-            window = sg.Window('Rendirse', layout)
+                     [sg.exit(), sg.Cancel()]]
+           window = sg.Window('Rendirse', layout)
         if event in ('Salir',None):
-           layout = [[sg.Text('¿Guardar partida?')],         
-                     [sg.Save(), sg.Cancel()]]      
-            window = sg.Window('Salir', layout)    
+           layout = [[sg.Text('¿Guardar partida?')],
+                     [sg.Save(), sg.Cancel()]]
+           window = sg.Window('Salir', layout)
         elif event == 'Total':
             filename = open('Puntajes_Total.txt','r+')
             modificar_dato(filename,puntaje)
