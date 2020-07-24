d83bb3dedcb2a4a3a3fd286c618ea070f341fafd
leodelmas00 <DelmasLeonardo@gmail.com>
2020-07-24 16:32:58 -0300

    Update Opciones
    size

--- Opciones.py
+++ Opciones.py
@@ -371,7 +371,7 @@ def Ventana_Opciones ():
     Lista_Letras = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Enie','O','P','Q','R','S','T','U','V','W','X','Y','Z']
     Diseño = [ [sg.Column((Layout_Main())),
                 sg.Column(Layout_Columna())] ]
-    window = sg.Window('Opciones', Diseño,font='Cambria')
+    window = sg.Window('Opciones', Diseño,font='Cambria',size=(808, 667))
     window.Read(timeout=1)[1]
     Dicc_Bolsa={"A":11,"B":3,"C":4,"D":4,"E":11,"F":2,"G":2,"H":2,"I":6,"J":2,"K":1,"L":4,"M":3,"N":5,
                       "Enie":1,"O":8,"P":2,"Q":1,"R":4,"S":7,"T":4,"U":6,"V":2,"W":1,"X":1,"Y":1,"Z":1}
