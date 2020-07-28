92e427c921eeeaf153123edd5dfe347c3a772423
MB-TAYLOR <55766201+MB-TAYLOR@users.noreply.github.com>
2020-07-28 19:48:33 -0300

    Nuevo Top

--- TabladePosiciones.py
+++ TabladePosiciones.py
@@ -1,38 +1,91 @@
+#!/usr/bin/env python
 import PySimpleGUI as sg
-sg.ChangeLookAndFeel('BrownBlue') # change style
-def genero_Top():
-    filename:str = None
-# string variables to shorten loop and menu code
-    menu_layout: list = ['Total','Facil','Medio','Dificil','Salir']
+import csv
 
-    layout: list = [[sg.Button(menu_layout[0]),sg.Button(menu_layout[1]),sg.Button(menu_layout[2]),sg.Button(menu_layout[3]),sg.Button(menu_layout[4])],
-                    [sg.Text('', font=('Consolas', 10), size=(90, 1), key='_INFO_')],
-                    [sg.Text(font=('Consolas', 12), size=(90, 25), key='_BODY_')]]
+# Show CSV data in Table
+sg.theme('DarkGrey2')
 
-    window: object = sg.Window('Top', layout=layout, margins=(0, 0), resizable=True, return_keyboard_events=True)
-    Tabla=window.FindElement('_BODY_')
+def Tabla():
+
+    data = []
+    data_facil = []
+    data_normal = []
+    data_dificil = []
+    data_personalizada = []
+    header_list = []
+    try:
+        with open("Archivo_Puntajes.csv", "r") as infile:
+            reader = csv.reader(infile)
+            header_list = next(reader)
+            header_list.insert(0, 'N°')
+            for row in reader:
+                if (len(row) > 0):
+                    if row[3] == 'Facil':
+                        data_facil.append(row)
+                    elif row[3] == 'Normal':
+                        data_normal.append(row)
+                    elif row[3] == 'Dificil':
+                        data_dificil.append(row)
+                    elif row[3] == 'Personalizada':
+                        data_personalizada.append(row)
+    except ValueError:
+            sg.popup_error('Error al leer: Archivo_Puntajes.csv')
+
+    def DataTotal(window,*Lista):
+        Total = []
+        Merge = []
+        for Dificultad in Lista:
+            print(Dificultad)
+            Merge.extend(Dificultad)
+            print(Merge)
+        Merge = sorted(Merge,key=lambda x:int(x[1]),reverse=True)
+        for x in range(10):
+            Total.append(Merge[x])
+            Total[x].insert(0, x+1)
+        window['info'].Update(Total)
+        for x in range(10):
+            Total[x].remove(x+1)
+
+    def DataMod(window,Lista):
+        Lista = sorted(Lista,key=lambda x:int(x[1]),reverse=True)
+        for x in range(len(Lista)):
+            Lista[x].insert(0, x+1)
+        window['info'].Update(Lista)
+        for x in range(len(Lista)):
+            Lista[x].remove(x+1)
+
+    sg.set_options(element_padding=(0, 0))
+
+    layout: list = [[sg.Button(button_text="Total",size=(17,2),pad=((0,14),(0,0)),key="Total"),sg.Button(button_text="Facil",size=(17,2),pad=((0,14),(0,0)),key="Facil"),sg.Button(button_text="Normal",size=(17,2),pad=((0,14),(0,0)),key="Normal"),sg.Button(button_text="Dificil",size=(17,2),pad=((0,14),(0,0)),key="Dificil"),sg.Button(button_text="Personalizada",size=(17,2),pad=((0,14),(0,0)),key="Personalizada")],
+                     [sg.Table(values=data,
+                            key='info',
+                            headings=header_list,
+                            def_col_width=17,
+                            auto_size_columns=False,
+                            hide_vertical_scroll=True,
+                            display_row_numbers=False,
+                            justification='center',
+                            alternating_row_color='Brown',
+                            row_height=50,
+                            num_rows=10)],
+                            [sg.Button(button_text="Salir",size=(20,2),key="Salir")]]
+
+
+    window = sg.Window('Top',layout,location=(200,50),size=(797,600),finalize=True)
     while True:
         event, values = window.read()
         if event in ('Salir',None):
             break
         elif event == 'Total':
-            filename = open('Puntajes_Total.txt','r')
-            Tabla.Update(filename.read())
-            filename.close()
-        elif event == 'Facil':
-            filename = open('Puntajes_Facil.txt','r')
-            Tabla.Update(filename.read())
-            filename.close()
-        elif event == 'Medio':
-            filename = open('Puntajes_Medio.txt','r')
-            Tabla.Update(filename.read())
-            filename.close()
+            DataTotal(window,data_facil,data_normal,data_dificil,data_personalizada)
+        elif event in ('Facil',values):
+            DataMod(window,data_facil)
+        elif event == 'Normal':
+            DataMod(window,data_normal)
         elif event == 'Dificil':
-            filename = open('Puntajes_Dificil.txt','r')
-            Tabla.Update(filename.read())
-            filename.close()
+            DataMod(window,data_dificil)
+        elif event == 'Personalizada':
+            DataMod(window,data_personalizada)
     window.close()
-    return(event)
-#ProgramaPrincipal-------------
-if __name__ == "__main__":
-    genero_Top()
+
+Tabla()
