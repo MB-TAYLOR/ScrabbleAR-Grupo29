21d4391a5a0b6d4dea6d4a1ea944ae8620680f4d
MarcoSilva <62036248+marcosilva1998@users.noreply.github.com>
2020-08-06 16:58:22 -0300

    Arreglo identificador

--- ScrabbleAR_py/Generadores.py
+++ ScrabbleAR_py/Generadores.py
@@ -25,32 +25,34 @@ def identificador_carpeta_error(ProgramaPrincipal):
         event=ProgramaPrincipal()
     except FileNotFoundError:
         tb = sys.exc_info()[2]
-        tbinfo = traceback.format_tb(tb)[2]
+        tbinfo = traceback.format_tb(tb)[3]
+        print(traceback.format_tb(tb)[3])
         ruta_archivo_error=tbinfo[tbinfo.find("(")+1:tbinfo.find(")")].strip(",'r'")
-        ruta_carpeta=ruta_archivo_error[:ruta_archivo_error.find(chr(92))]
-        sg.popup_error("Error al intentar acceder al archivo de la siguiente ruta :",ruta_archivo_error,"\nRevise que el archivo se encuentre en la carpeta",ruta_carpeta,title='Error')
+        ruta_archivo_error=ruta_archivo_error.strip("corrector_paths('")
+        ruta_carpeta=ruta_archivo_error[:ruta_archivo_error.find(os.sep)]
+        sg.popup_error("Error al intentar acceder al archivo de la siguiente ruta :",corrector_paths(ruta_archivo_error),"\nRevise que el archivo se encuentre en la carpeta",ruta_carpeta,title='Error')
     except tkinter.TclError:
         Direciones_error={"inicio":corrector_paths(r'ScrabbleAR_Imagenes_png\icono_inicio.png'),"yellow":corrector_paths(r'ScrabbleAR_Imagenes_png\icono3.png'),"red":corrector_paths(r'ScrabbleAR_Imagenes_png\icono_x2.png'),
         "green":corrector_paths(r'ScrabbleAR_Imagenes_png\icono_-3.png'),"blue":corrector_paths(r'ScrabbleAR_Imagenes_png\icono_-2.png'),"white":corrector_paths(r'ScrabbleAR_Imagenes_png\modelo_ficha.png')}
         tb = sys.exc_info()[2]
-        if("window[coord].update" in traceback.format_tb(tb)[2]):
-            tbinfo = traceback.format_tb(tb)[2]
+        if("window[coord].update" in traceback.format_tb(tb)[3]):
+            tbinfo = traceback.format_tb(tb)[3]
             ruta_archivo_error=tbinfo[tbinfo.find("(")+1:tbinfo.find(")")].strip(",'r'")
             ruta_archivo_error=ruta_archivo_error[:ruta_archivo_error.find(",")]
             ruta_archivo_error=ruta_archivo_error[ruta_archivo_error.find("=")+1:]
             ruta_carpeta=Direciones_error[ruta_archivo_error]
-            ruta_carpeta=ruta_carpeta[:ruta_carpeta.find(chr(92))]
-            sg.popup_error("Error al intentar acceder a la imagen de la siguiente ruta :",Direciones_error[ruta_archivo_error] ,"\nRevise que la imagen se encuentre el la carpeta: ",ruta_carpeta ,title='Error')
+            ruta_capeta=ruta_carpeta.split(str(os.sep))
+            sg.popup_error("Error al intentar acceder a la imagen de la siguiente ruta :",Direciones_error[ruta_archivo_error] ,"\nRevise que la imagen se encuentre el la carpeta: ",ruta_capeta[len(ruta_capeta)-2] ,title='Error')
         else:
             sg.popup_error("Falta alguna imagen en la carpeta ScrabbleAR_Imagenes_png",title='Error')
     except UnicodeDecodeError:
         tb = sys.exc_info()[2]
-        if "playsound" in traceback.format_tb(tb)[2]:
-            tbinfo = traceback.format_tb(tb)[2]
+        if "playsound" in traceback.format_tb(tb)[1]:
+            tbinfo = traceback.format_tb(tb)[1]
             ruta_archivo_error=tbinfo[tbinfo.find("(")+1:tbinfo.find(")")].strip(",'r'")
-            ruta_archivo_error=ruta_archivo_error[:ruta_archivo_error.find("'")]
-            ruta_carpeta=ruta_archivo_error[:ruta_archivo_error.find(chr(92))]
-            sg.popup_error("Error al intentar acceder al archivo de audio de la siguiente ruta :",ruta_archivo_error,"\nRevise que el archivo de audio se encuentre el la carpeta: ",ruta_carpeta ,title='Error')
+            ruta_archivo_error=ruta_archivo_error.strip("corrector_paths('")
+            ruta_carpeta=ruta_archivo_error[:ruta_archivo_error.find(os.sep)]
+            sg.popup_error("Error al intentar acceder al archivo de audio de la siguiente ruta :",corrector_paths(ruta_archivo_error),"\nRevise que el archivo de audio se encuentre el la carpeta: ",ruta_carpeta ,title='Error')
         else:
             sg.popup_error("Falta un archivo de audio , revise la carpeta ScrabbleAR_Sonidos ",title='Error')
     #except:
